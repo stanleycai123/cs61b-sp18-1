fa0d4f4a37dd98b1eceb4b105c6028979f28cc74
Junhao Wang <junhaowww@outlook.com>
2019-03-28 13:40:47 -0700

    update HelloNumbers.java

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -2,7 +2,7 @@ public class HelloNumbers {
   public static void main(String[] args) {
     int x = 0;
     while (x < 10) {
-      System.out.println(x + " ");
+      System.out.print(x + " ");
       x = x + 1;
     }
   }
