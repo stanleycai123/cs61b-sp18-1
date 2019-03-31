56a5dc7b9f0c99b1120f8dcb15e92b8eedcc3a43
Junhao Wang <junhaowww@outlook.com>
2019-03-30 22:30:09 -0700

    Update SLList (size, addLast)

--- classcode/w2/SLList.java
+++ classcode/w2/SLList.java
@@ -3,7 +3,7 @@
  */
 
 public class SLList {
-  public IntNode first;
+  private IntNode first;
 
   public SLList(int x) {
     first = new IntNode(x, null);
@@ -26,24 +26,66 @@ public class SLList {
   public int getFirst() {
     return first.item;
   }
-}
 
-
-class IntNode {
-  public int item;
-  public IntNode next;
-
-  public IntNode(int item, IntNode next) {
-    this.item = item;
-    this.next = next;
+  /** .addLast */
+  public void addLast(int x) {
+    /* Assume there is already at least one node */
+    IntNode p = first;
+    while (p.next != null) { /* Move p until it reaches the end of the list */
+      p = p.next;
+    } /* now p points to the last element rather than null */
+    p.next = new IntNode(x, null);
   }
 
-  /** Tricky - addFirst for IntList (IntNode) */
-  public void addFirst(int x) {
-    // this = new IntNode(x, this); not okay!
-    /* change the first object */
-    IntNode node2 = new IntNode(item, next);
-    item = x;
-    next = node2;
+  /** .size() */
+  public int size() {
+    IntNode p = first;
+    int totalSize = 0;
+    while (p != null) {
+      totalSize += 1;
+      p = p.next;
+    }
+    return totalSize;
+  }
+
+  /** Recursive version
+   *  Tricky. 'cause SLList is not recursive!
+   */
+  public int size_re() { // public method that uses the private method
+    return size(first);
+  }
+  /** Returns the size of the list that starts at IntNode p (common pattern) */
+  /** Private Helper Method */
+  private static int size(IntNode p) { /* same name but different signature - overload */
+    if (p == null) { /* or p.next == null */
+      return 0;      /* or return 1; */
+    }
+    return 1 + size(p.next);
+  }
+
+
+
+  /**
+   * IntNode
+   * private: cannot access IntNode outside SLList
+   * static:  if code in IntNode never looks outwards
+   */
+  private class IntNode {
+    public int item;
+    public IntNode next;
+
+    public IntNode(int item, IntNode next) {
+      this.item = item;
+      this.next = next;
+    }
+
+    /** Tricky - addFirst for IntList (IntNode) */
+    public void addFirst(int x) {
+      // this = new IntNode(x, this); not okay!
+      /* change the first object */
+      IntNode node2 = new IntNode(item, next);
+      item = x;
+      next = node2;
+    }
   }
 }
