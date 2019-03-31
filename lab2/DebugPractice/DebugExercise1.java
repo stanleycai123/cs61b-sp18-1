abf27ebe07b4b53fc1c83fc8f31e1bea4f4b0989
Junhao Wang <junhaowww@outlook.com>
2019-03-30 17:15:49 -0700

    finish lab2 setup

--- lab2/DebugPractice/DebugExercise1.java
+++ lab2/DebugPractice/DebugExercise1.java
@@ -3,26 +3,27 @@
  * feature work.
  */
 public class DebugExercise1 {
-    public static int divideThenRound(int top, int bottom) {
-        int quotient = top / bottom;
-        int result = Math.round(quotient);
-        return result;
-    }
+  public static int divideThenRound(int top, int bottom) {
+    float quotient = (float) top / bottom;
+    int result = Math.round(quotient);
+    return result;
+  }
 
-    public static void main(String[] args) {
-        int t = 10;
-        int b = 2;
-        int result = divideThenRound(t, b);
-        System.out.println("round(" + t + "/" + b + ")=" + result);
+  public static void main(String[] args) {
+    int t = 10;
+    int b = 2;
+    int result = divideThenRound(t, b);
 
-        int t2 = 9;
-        int b2 = 4;
-        int result2 = divideThenRound(t2, b2);
-        System.out.println("round(" + t2 + "/" + b2 + ")=" + result2);
+    System.out.println("round(" + t + "/" + b + ")=" + result);
 
-        int t3 = 3;
-        int b3 = 4;
-        int result3 = divideThenRound(t3, b3);
-        System.out.println("round(" + t3 + "/" + b3 + ")=" + result3);
-    }
+    int t2 = 9;
+    int b2 = 4;
+    int result2 = divideThenRound(t2, b2);
+    System.out.println("round(" + t2 + "/" + b2 + ")=" + result2);
+
+    int t3 = 3;
+    int b3 = 4;
+    int result3 = divideThenRound(t3, b3);
+    System.out.println("round(" + t3 + "/" + b3 + ")=" + result3);
+  }
 }
