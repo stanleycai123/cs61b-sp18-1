d3f474a564de8f97d2eb9ce654eb477602706573
Junhao Wang <junhaowww@outlook.com>
2019-04-02 00:45:02 -0700

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cs61b-sp18
+
 My repo for CS61B
+
+Study Notes: [blog](https://www.junhaow.com/studynotes/#cs61b)
+
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,7 @@
+/**
+ * Created by JunhaoW on 04/01/2019
+ */
+package PACKAGE_NAME;
+
+public class LinkedListDeque {
+}
