26ca0a6e2d171995dcb0add8c576b77a68e91ff2
Junhao Wang <junhaowww@outlook.com>
2019-04-04 18:34:04 -0700

    Add ArrayDeque.java ArrayDequeTest.java (addFirst/addLast/resize)

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,7 +1,114 @@
 /**
  * Created by JunhaoW on 04/03/2019
  */
-package PACKAGE_NAME;
 
-public class ArrayDeque {
+public class ArrayDeque<T> {
+
+  /** Field */
+  private int size;
+  private int nextFirst;
+  private int nextLast;
+  private T[] items;
+
+  private int initSize = 4;
+
+  /**
+   * Constructor
+   */
+  public ArrayDeque() {
+    items = (T[]) new Object[initSize];
+    int mid = items.length / 2;
+    nextFirst = mid - 1;
+    nextLast = mid;
+    size = 0;
+  }
+
+  /**
+   * API Method
+   */
+
+  /** size */
+  public int size() {
+    return size;
+  }
+
+  /** get */
+  public int get(int index) {
+    return 0;
+  }
+
+  /** resize */
+  public void resize(int capacity) {
+    T[] newItems = (T[]) new Object[capacity];
+    //  0  1  2  3  4  5  6  7
+    //       [x][x][x][x]       items (copy array)
+    // [ ][ ][ ][ ][ ][ ][ ][ ] newItems
+    //    [x][x][x]   if initSize is an odd number
+    // [ ][ ][ ][ ][ ][ ]
+    int startPos = newItems.length / 2 - size / 2; /* draw a picture */
+    /** Copy */
+    /* // stupid way
+    int oldIndex = (nextFirst + 1 > size - 1) ? (0) : (nextFirst + 1);
+    // if nextF points at the last position, it means we need to start from 0
+    */
+    int oldIndex = nextFirst + 1; /* Use MOD operation instead */
+    int newIndex = startPos; /* newIndex for newItems */
+    int count = 0;
+    while (count < size) { /* yes! the original size! */
+      newItems[newIndex] = items[oldIndex % size]; /* 3 + 1 = 4, 4 % 4 = 0 */
+      oldIndex++; newIndex++;
+      count++;
+    }
+    /** Set new fields */
+    items = newItems;
+    // size = capacity; bug! we don't need to change size!
+    nextFirst = startPos - 1;
+    nextLast = newIndex; /* or nextFirst + size */
+  }
+
+  /** addFirst */
+  public void addFirst(T item) {
+    if (size == items.length) { /** FULL! */
+      resize(items.length * 2);
+    }
+    items[nextFirst] = item;
+    nextFirst = ((nextFirst - 1) + items.length) % items.length;
+    /* since in java, -5 % 6 == -5, we need to add items.length */
+    size++;
+  }
+
+  /** addLast */
+  public void addLast(T item) {
+    if (size == items.length) {
+      resize(items.length * 2);
+    }
+    items[nextLast] = item;
+    nextLast = (nextLast + 1) % items.length; /* nextLast += 1 */
+    size++;
+  }
+
+  /** removeFirst */
+  public T removeFirst() {
+    return null;
+  }
+
+  /** removeLast */
+  public T removeLast() {
+    return null;
+  }
+
+  /** printDeque */
+  public void printDeque() {
+
+  }
+
+  /**
+   * getItem - for testing
+   */
+  public T getItem(int index) {
+    return items[index];
+  }
+  public int getLength() {
+    return items.length;
+  }
 }
---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,90 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by JunhaoW on 04/04/2019
+ */
+
+public class ArrayDequeTest {
+
+  @Test
+  public void testResize() {
+    ArrayDeque<Integer> D = new ArrayDeque<>();
+    if (D.getLength() == 4) {
+      /** Case 1 - addFirst only */
+      // [2][1][4][3]
+      D.addFirst(1);
+      D.addFirst(2);
+      D.addFirst(3);
+      D.addFirst(4);
+      assertEquals("Before resizing", Integer.valueOf(4), D.getItem(2));
+      // [ ][5][2][1][4][3][6][ ]
+      D.addFirst(5);
+      assertEquals("After resizing - length", 8, D.getLength());
+      assertEquals("After resizing - size", 5, D.size());
+      assertEquals("After resizing - 5", Integer.valueOf(5), D.getItem(1));
+      D.addLast(6);
+      assertEquals("After resizing - 6", Integer.valueOf(6), D.getItem(6));
+
+      /** Case 2 - addLast only */
+      D = new ArrayDeque<>();
+      // [3][4][1][2]
+      D.addLast(1);
+      D.addLast(2);
+      D.addLast(3);
+      D.addLast(4);
+      assertEquals("Before resizing", Integer.valueOf(4), D.getItem(1));
+      // [ ][6][3][4][1][2][5][ ]
+      D.addLast(5);
+      assertEquals("After resizing - length", 8, D.getLength());
+      assertEquals("After resizing - size", 5, D.size());
+      assertEquals("After resizing - 5", Integer.valueOf(5), D.getItem(6));
+      D.addFirst(6);
+      assertEquals("After resizing - 6", Integer.valueOf(6), D.getItem(1));
+
+      /** Case 3 - Both */
+      D = new ArrayDeque<>();
+      // [4][1][2][3]
+      D.addFirst(1);
+      D.addLast(2);
+      D.addLast(3);
+      D.addFirst(4);
+      assertEquals("Before resizing", Integer.valueOf(3), D.getItem(3));
+      // [ ][5][4][1][2][3][6][ ]
+      D.addFirst(5);
+      assertEquals("After resizing - length", 8, D.getLength());
+      assertEquals("After resizing - size", 5, D.size());
+      assertEquals("After resizing - 5", Integer.valueOf(5), D.getItem(1));
+      D.addLast(6);
+      assertEquals("After resizing - 6", Integer.valueOf(6), D.getItem(6));
+    }
+  }
+
+  @Test
+  public void testAddFirstLast() {
+    ArrayDeque<Integer> D1 = new ArrayDeque<>();
+    if (D1.getLength() == 4) {
+      // [9][1][ ][8]
+      D1.addFirst(1);
+      assertEquals(Integer.valueOf(1), D1.getItem(1));
+      D1.addFirst(9);
+      assertEquals(Integer.valueOf(9), D1.getItem(0));
+      D1.addFirst(8);
+      assertEquals(Integer.valueOf(8), D1.getItem(3));
+      D1.addFirst(7);
+      assertEquals(Integer.valueOf(7), D1.getItem(2));
+
+      ArrayDeque<Integer> D2 = new ArrayDeque<>();
+      // [5][6][7][1]
+      D2.addLast(7);
+      assertEquals(Integer.valueOf(7), D2.getItem(2));
+      D2.addLast(1);
+      assertEquals(Integer.valueOf(1), D2.getItem(3));
+      D2.addLast(5);
+      assertEquals(Integer.valueOf(5), D2.getItem(0));
+      D2.addLast(6);
+      assertEquals(Integer.valueOf(6), D2.getItem(1));
+    }
+  }
+}
