c438073bf687a785afa81a042ff929c423648935
Junhao Wang <junhaowww@outlook.com>
2019-05-18 22:51:16 -0700

    Add RabinKarpAlgorithm

--- clab8/RabinKarpAlgorithm.java
+++ clab8/RabinKarpAlgorithm.java
@@ -1,12 +1,47 @@
 public class RabinKarpAlgorithm {
 
-
-    /**
-     * This algorithm returns the starting index of the matching substring.
-     * This method will return -1 if no matching substring is found, or if the input is invalid.
-     */
-    public static int rabinKarp(String input, String pattern) {
-        return -1;
+  public static int bruteForce(String input, String pattern) {
+    if (pattern.length() > input.length()) return -1;
+    int n = input.length();
+    int m = pattern.length();
+    for (int i = 0; i < n - m + 1; i += 1) {
+      boolean found = true;
+      for (int j = 0; j < m; j += 1) {
+        if (input.charAt(i + j) != pattern.charAt(j)) {
+          found = false;
+          break;
+        }
+      }
+      if (found) return i;
     }
+    return -1;
+  }
+
+  /**
+   * This algorithm returns the starting index of the matching substring.
+   * This method will return -1 if no matching substring is found, or if the input is invalid.
+   */
+  public static int rabinKarp(String input, String pattern) {
+    if (pattern.length() > input.length()) return -1;
+    int n = input.length();
+    int m = pattern.length();
+
+    RollingString patternRoll = new RollingString(pattern, m);
+    RollingString inputRoll = new RollingString(input.substring(0, m), m);
+    int patternHash = patternRoll.hashCode();
+    // 0 1 2 3 4
+    // a b c d e
+    for (int i = 0; i < n - m + 1; i += 1) {
+      int inputHash = inputRoll.hashCode();   // constant
+      if (inputHash == patternHash) {
+        // match
+        if (inputRoll.equals(patternRoll)) {
+          return i;
+        }
+      }
+      if (i + m < n)  inputRoll.addChar(input.charAt(i + m));   // constant
+    }
+    return -1;
+  }
 
 }
--- clab8/RabinKarpAlgorithmTests.java
+++ clab8/RabinKarpAlgorithmTests.java
@@ -1,11 +1,18 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
+// h - 104
+// e - 101
+// l - 108
+// o - 111
 
 public class RabinKarpAlgorithmTests {
     @Test
     public void basic() {
         String input = "hello";
         String pattern = "ell";
+        assertEquals(1, RabinKarpAlgorithm.bruteForce(input, pattern));
         assertEquals(1, RabinKarpAlgorithm.rabinKarp(input, pattern));
     }
 }
